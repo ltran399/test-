fc9c6f6cac00478a85ac7948ffefa894f0d6c61c
ltran399 <87052420+ltran399@users.noreply.github.com>
2024-04-08 20:08:10 +0700

    Create Place.java

---
+++ Place.java
@@ -0,0 +1,100 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Place {
+    private final int x;
+    private final int y;
+    private Set<String> services;
+
+    public Place(int x, int y, Set<String> services) {
+        this.x = x;
+        this.y = y;
+        this.services = new HashSet<>(services);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Set<String> getServices() {
+        return new HashSet<>(services);
+    }
+
+    public void setServices(Set<String> services) {
+        this.services = new HashSet<>(services);
+    }
+}
+
+class QuadtreeNode {
+    private static final int MAX_CAPACITY = 4;
+    boolean isDivided;
+    int x, y, width, height;
+    Set<Place> places;
+    QuadtreeNode[] children;
+
+    public QuadtreeNode(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.places = new HashSet<>();
+        this.isDivided = false;
+    }
+
+    void subdivide() {
+        int subWidth = width / 2;
+        int subHeight = height / 2;
+        children = new QuadtreeNode[4];
+        children[0] = new QuadtreeNode(x, y, subWidth, subHeight);
+        children[1] = new QuadtreeNode(x + subWidth, y, subWidth, subHeight);
+        children[2] = new QuadtreeNode(x, y + subHeight, subWidth, subHeight);
+        children[3] = new QuadtreeNode(x + subWidth, y + subHeight, subWidth, subHeight);
+        isDivided = true;
+
+        for (Place place : places) {
+            int i = (place.getX() >= x + subWidth ? 1 : 0) + (place.getY() >= y + subHeight ? 2 : 0);
+            children[i].insert(place);
+        }
+        places.clear();
+    }
+
+    void insert(Place place) {
+        if (isDivided) {
+            int i = (place.getX() >= x + width / 2 ? 1 : 0) + (place.getY() >= y + height / 2 ? 2 : 0);
+            children[i].insert(place);
+        } else {
+            places.add(place);
+            if (places.size() > MAX_CAPACITY) {
+                subdivide();
+            }
+        }
+    }
+
+    void search(int searchX, int searchY, int searchWidth, int searchHeight, String serviceType, Set<Place> results) {
+        if (!intersects(searchX, searchY, searchWidth, searchHeight)) {
+            return;
+        }
+        if (isDivided) {
+            for (QuadtreeNode child : children) {
+                child.search(searchX, searchY, searchWidth, searchHeight, serviceType, results);
+            }
+        } else {
+            for (Place place : places) {
+                if (place.getX() >= searchX && place.getX() <= searchX + searchWidth &&
+                    place.getY() >= searchY && place.getY() <= searchY + searchHeight &&
+                    place.getServices().contains(serviceType)) {
+                    results.add(place);
+                }
+            }
+        }
+    }
+
+    boolean intersects(int searchX, int searchY, int searchWidth, int searchHeight) {
+        return !(x + width < searchX || x > searchX + searchWidth ||
+                 y + height < searchY || y > searchY + searchHeight);
+    }
+}
